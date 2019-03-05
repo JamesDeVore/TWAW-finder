1ba8938dc32dd808723bc04af3b2272861887cc9
James DeVore <jdevore4592@gmail.com>
2019-03-05 13:06:09 -0500

    serve index for production

--- index.js
+++ index.js
@@ -14,18 +14,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 router(app);
 
-// if (process.env.NODE_ENV === "production") {
-//   // Express will serve up production assets
-//   // like our main.js file, or main.css file!
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
 
-//   // Express will serve up the index.html file
-//   // if it doesn't recognize the route
-//   const path = require("path");
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Server Setup
 const port = process.env.PORT || 5000;
