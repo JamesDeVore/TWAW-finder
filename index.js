962ad0570f719e43fb689bee90a130821c1bfc35
James DeVore <jdevore4592@gmail.com>
2019-03-05 13:24:14 -0500

    restructuring index.js

--- index.js
+++ index.js
@@ -14,16 +14,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 router(app);
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
+//production mode
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
 }
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
+})
 
 // Server Setup
 const port = process.env.PORT || 5000;
