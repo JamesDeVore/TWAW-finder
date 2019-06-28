818e5f38844fd02e381e4ddbaa8289e3f9c50065
James DeVore <jdevore4592@gmail.com>
2019-06-28 13:35:59 -0400

    updating results page

--- client/src/components/Questions/Results.js
+++ client/src/components/Questions/Results.js
@@ -6,15 +6,18 @@ export default function Results(props) {
   return (
     <div className="gunResult">
       <div className="gunTitle">
-        <h2>Model: {gun.GUN}</h2>
+        <h2>{gun.GUN}</h2>
       </div>
       <div className="gunDetails">
         <img src={gun["Image Links"]} className="gunImage" alt=""/>
         <div className="gunStats">
           <ul>
+            <li>Manufacturer: {gun.Manufacturer}</li>
             <li>Caliber: {gun.caliber}</li>
-            <li>stat</li>
-            <li>stat</li>
+            <li>MSRP: ${gun.Price}</li>
+            <li>Frame Material: {gun.FrameMaterial}</li>
+            <li>Action type: {gun.ActionType}</li>
+            <li>Link: <a href="google.com">Link here</a></li>
           </ul>
         </div>
       </div>
